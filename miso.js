b901d81b96195b84b97a20801b3640bb8b318255
codestar141 <taiyet96@gmail.com>
2024-08-19 16:50:37 +0100

    new commit

--- miso.js
+++ miso.js
@@ -1,3 +1,15 @@
-function goToSignup() {
-    window.location.href = 'signup.html';
+const menuButton = document.querySelector("#menu-button");
+const dropDown = document.querySelector(".dropdown");
+const signUp = document.querySelector("#sign-up");
+
+function handleOpenDropdown(){
+    dropDown.classList.add("open");
 }
+
+function handleCloseDropdown(){
+    dropDown.classList.remove("open");
+}
+
+signUp.addEventListener("click", () => {
+    window.location.href = "signup.html";
+});
